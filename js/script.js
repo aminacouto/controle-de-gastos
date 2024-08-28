fab3fc65dc0a407befd3400f8c6771142908f9b5
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2024-08-28 17:52:44 -0300

    ajustes de posições, várias coisas
    falta transições

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//ANIMAÇÃO TELA DE LOGIN
+
+const btnEntrar = document.querySelector("#btnEntrar");
+const btnCadastar = document.querySelector("#btnCadastrar");
+
+const body = document.querySelector('body');
+
+btnEntrar.addEventListener ("click", function(){
+    body.className = "entrar-js";
+});
+
+btnCadastar.addEventListener ("click", function (){
+    body.className = "cadastrar-js";
+})
