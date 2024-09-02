16a6faad6ac8a50a5ed5a5293553a6cd6eb202fb
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2024-09-02 16:16:36 -0300

    css

--- js/script.js
+++ js/script.js
@@ -12,3 +12,17 @@ btnEntrar.addEventListener ("click", function(){
 btnCadastar.addEventListener ("click", function (){
     body.className = "cadastrar-js";
 })
+
+
+//VALIDAÇÃO DOS BOTÕES
+
+function logar() {
+    var login = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+
+    if(login == "admin" && senha == "admin") {
+        location.href = "paginaInicial.html";
+    } else {
+        alert ('USUÁRIO OU SENHA INCORRETA')
+    }
+}
