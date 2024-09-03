205d8d86646e074085f3756586e8c50f68a620d5
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2024-09-03 17:13:36 -0300

    validação dos inputs
    email e senha de logar

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyB3JQBCKeBIN34LDQwIggL6WQgRkhYcPFs",
+    authDomain: "controle-de-gastos-123e0.firebaseapp.com",
+    projectId: "controle-de-gastos-123e0",
+    storageBucket: "controle-de-gastos-123e0.appspot.com",
+    messagingSenderId: "637250162104",
+    appId: "1:637250162104:web:304225a0b9595ca2d7134d"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  firebaseConfig.auth().signInWithEmailAndPassword("ami@email.com", "123456").tnen(response =>{
+    console.log('ok', response)
+  }).catch(error =>{
+    console.log('erro', error)
+  });
--- js/script.js
+++ js/script.js
@@ -13,16 +13,40 @@ btnCadastar.addEventListener ("click", function (){
     body.className = "cadastrar-js";
 })
 
+/*REALIZAR O LOGIN - ir para a pag principal */
+function logar(event){
+    event.preventDefault();
+    window.location.href = "paginaPrincipal.html";
+}
 
 //VALIDAÇÃO DOS BOTÕES
+function validacao(){
+    const emailValido = isEmailValido();
+    document.getElementById('novaSenha').disabled = !emailValido;
 
-function logar() {
-    var login = document.getElementById("email").value;
-    var senha = document.getElementById("senha").value;
-
-    if(login == "admin" && senha == "admin") {
-        location.href = "paginaInicial.html";
-    } else {
-        alert ('USUÁRIO OU SENHA INCORRETA')
-    }
+    const senhaValida = isSenhaValida();
+    document.getElementById('botaoEntrar').disabled = !emailValido || !senhaValida;
 }
+
+/*VALIDAR EMAIL*/
+function isEmailValido(){
+    const email = document.getElementById("emailLogin").value;
+    if(!email) {
+        return false;
+    } 
+    return validaEmail(email);
+} 
+/*VALIDAR SENHA*/
+function isSenhaValida(){
+    const senha = document.getElementById('senhaLogin').value;
+    if(!senha) {
+        return false;
+    }
+    return true;
+}
+
+function validaEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+
