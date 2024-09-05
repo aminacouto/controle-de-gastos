4affd9f6e1371eb200782cf657dd412a594d53f8
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2024-09-05 17:32:35 -0300

    reajustando

--- js/script.js
+++ js/script.js
@@ -12,70 +12,27 @@ btnCadastar.addEventListener("click", function () {
     body.className = "cadastrar-js";
 })
 
-/*LOGIN */
-//VALIDAÇÃO DOS BOTÕES
-function validacao() {
-    const emailValido = isEmailValido();
-    document.getElementById('novaSenha').disabled = !emailValido;
 
-    const senhaValida = isSenhaValida();
-    document.getElementById('botaoEntrar').disabled = !emailValido || !senhaValida;
-}
-/*VALIDAR EMAIL*/
-function isEmailValido() {
-    const email = form.emailLogin().value;
-    if (!email) {
-        return false;
-    }
-    return validaEmail(email);
-}
-/*VALIDAR SENHA*/
-function isSenhaValida() {
-    const senha = form.senhaLogin().value;
-    if (!senha) {
-        return false;
-    }
-    return true;
-}
+function cadastrarUsuario() {
+    /*const nome = document.getElementById("nome").value;
+    const email = document.getElementById("emailCadastrar").value;
+    const senha = document.getElementById("senhaCadastrar").value;
 
-/*CADASTRO */
-// VALIDAÇÃO DOS BOTÕES
-function cadastrarUsuario(){
-    window.location.href = "../paginaPrincipal"
-}
-function validacaoCadastro() {
-    const emailValido = isEmailCadastroValido();
-    const senhaValida = isSenhaCadastroValida();
+    if (nome === '' || email === '' || senha === "") {
+        alert("Por favor, preencha todos os campos antes de continuar.")
+    } else {
+        window.location.href = "../paginaPrincipal.html"
+    }*/
+    window.location.href = "../paginaPrincipal.html"
+};
 
-    document.getElementById('botaoCadastrar').disabled = !emailValido || !senhaValida;
-}
-
-/* VALIDAR EMAIL DE CADASTRO */
-function isEmailCadastroValido() {
-    const email = form.emailCadastro().value;
-    if (!email) {
-        return false;
-    }
-    return validaEmail(email);
-}
-
-/* VALIDAR SENHA DE CADASTRO */
-function isSenhaCadastroValida() {
-    const senha = form.senhaCadastro().value;
-    if (!senha) {
-        return false;
-    }
-    return true;
-}
-
-function validaEmail(email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
-
-const form = {
-    emailLogin: () => document.getElementById("emailLogin"),
-    senhaLogin: () => document.getElementById("senhaLogin"),
-
-    emailCadastro: () => document.getElementById("emailCadastrar"),
-    senhaCadastro: () => document.getElementById("senhaCadastrar")
+function login() {
+    /*const emailLogin = document.getElementById("emailLogin").value;
+    const senhaLogin = document.getElementById("senhaLogin").value;
+    if (emailLogin === "" || senhaLogin === "") {
+        alert("Por favor, preencha todos os campos antes de continuar.");
+    } else {
+        window.location.href = "../paginaPrincipal.html"
+    };*/
+    window.location.href = "../paginaPrincipal.html"
 }
