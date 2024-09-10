b6d04e2754f2fe62d0e9edfa6df8d132f6d98d42
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2024-09-10 11:24:39 -0300

    validação

--- js/firebase.js
+++ js/firebase.js
@@ -2,6 +2,32 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, on
 
 const auth = getAuth();
 
+function verificarCampos() {
+    const emailLogin = document.getElementById("emailLogin");
+    const senhaLogin = document.getElementById("senhaLogin");
+    const botao = document.getElementById("botaoEntrar");
+
+    const emailCadastrar = document.getElementById("emailCadastrar");
+    const senhaCadastrar = document.getElementById("senhaCadastrar");
+    const botaoCadastrar = document.getElementById("botaoCadastrar");
+
+    if (emailLogin && senhaLogin && botaoEntrar) {
+        if (emailLogin.value === "" || senhaLogin.value === "") {
+            botaoEntrar.disabled = true;
+        } else {
+            botaoEntrar.disabled = false;
+        }
+    }
+
+    if (emailCadastrar && senhaCadastrar && botaoCadastrar) {
+        if (emailCadastrar.value === "" || senhaCadastrar.value === "") {
+            botaoCadastrar.disabled = true;
+        } else {
+            botaoCadastrar.disabled = false;
+        }
+    }
+}
+
 export function cadastrarUsuario() {
     const email = document.getElementById("emailCadastrar").value;
     const senha = document.getElementById("senhaCadastrar").value;
@@ -21,12 +47,18 @@ export function cadastrarUsuario() {
         });
 };
 
+
 export function login() {
     const email = document.getElementById("emailLogin").value;
     const senha = document.getElementById("senhaLogin").value;
+    const span = document.getElementById("erro");
+    const inputEmail = document.getElementById("emailLogin");
+    const inputSenha = document.getElementById("senhaLogin");
 
     if (email === "" || senha === "") {
-        alert("Por favor, preencha todos os campos antes de continuar.");
+        span.style.display = "block";
+        inputEmail.style.border = "1px solid red";
+        inputSenha.style.border = "1px solid red";
         return
     };
 
@@ -36,10 +68,19 @@ export function login() {
         })
         .catch((error) => {
             const errorMessage = error.message;
-            alert("Erro no login: " + errorMessage);
+            span.style.display = "block";
+            inputEmail.style.border = "1px solid red";
+            inputSenha.style.border = "1px solid red";
         });
 }
 
 document.querySelector(".formulario").addEventListener("submit", cadastrarUsuario);
 document.querySelector(".formulario").addEventListener("submit", login);
 
+//  verificar os campos sempre que o usuário digitar
+document.getElementById("emailLogin").addEventListener("input", verificarCampos);
+document.getElementById("senhaLogin").addEventListener("input", verificarCampos);
+
+document.getElementById("emailCadastrar").addEventListener("input", verificarCampos);
+document.getElementById("senhaCadastrar").addEventListener("input", verificarCampos);
+verificarCampos();
