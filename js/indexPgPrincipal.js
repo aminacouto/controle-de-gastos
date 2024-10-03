29ec4e8e5e96b8dee11890cd9ab04937ec12d1dc
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2024-10-03 15:57:29 -0300

    implementando formulario e lista

--- js/indexPgPrincipal.js
+++ js/indexPgPrincipal.js
@@ -6,3 +6,35 @@ function abreMenu() {
         menu.style.display = 'block';
     }
 }
+
+
+/**/
+function addGasto() {
+    const nomeGasto = document.getElementById('nomeGasto').value;
+    const valorGasto = document.getElementById('valorGasto').value;
+    const tipoGasto = document.getElementById('tipoGasto').value;
+    const dataGasto = document.getElementById('dataGasto').value;
+
+    // Verificando se os campos foram preenchidos
+    if (nomeGasto && valorGasto && tipoGasto !== "Tipo") {
+        // Criando o novo item da lista de gastos
+        const novoGasto = document.createElement('section');
+        novoGasto.classList.add('gastoItem');
+
+        // Inserindo o conteúdo no item
+        novoGasto.innerHTML = `
+        <h1>${nomeGasto}</h1>
+        <p>Valor: R$ ${valorGasto} (${tipoGasto})</p>`;
+
+        // Adicionando o novo item na lista de gastos
+        document.querySelector('.listaGastos').appendChild(novoGasto);
+
+        // Limpar os campos do formulário após adicionar
+        document.getElementById('nomeGasto').value = '';
+        document.getElementById('valorGasto').value = '';
+        document.getElementById('tipoGasto').value = 'Tipo';
+
+    } else {
+        alert("Preencha todos os campos!");
+    }
+}
